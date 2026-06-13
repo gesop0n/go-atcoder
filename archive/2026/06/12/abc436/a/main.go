582902bc4160092a6e17a7d227bd40621dec324f
gesop0n <ishikuro6.2@gmail.com>
2026-06-13 15:14:01 +0900

    ABC436no

---
+++ archive/2026/06/12/abc436/a/main.go
@@ -0,0 +1,18 @@
+// https://atcoder.jp/contests/abc436/tasks/abc436_a
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var n int
+	var s string
+	fmt.Scan(&n)
+	fmt.Scan(&s)
+	
+
+	forward := strings.Repeat("o", n - len(s))
+	fmt.Println(forward+s)
+}
